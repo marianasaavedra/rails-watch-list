7197c064eb891cbe30e38caa2c7d6dd84cd558ea
mariana saavedra <marianadsbs@gmail.com>
2021-05-28 13:12:57 +0100

    adding new images and style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
+  before_action :set_list, only: %i[show destroy]
 
   def index
     @lists = List.all
@@ -19,6 +19,12 @@ class ListsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @list.destroy
+
+    redirect_to lists_path, notice: 'List was successfully destroyed'
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,11 @@
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import "channels";
-import { initTyped } from '../components/typed';
 
 Rails.start();
 Turbolinks.start();
 
 document.addEventListener('turbolinks:load', () => {
-  initTyped();
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
